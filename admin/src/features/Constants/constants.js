f05d994babf943d442e92dd6a9d6343c82e94be9
Nguyen Manh Truong <gnourt2112000@gmail.com>
2022-01-11 19:16:33 +0700

    fix

--- admin/src/features/Constants/constants.js
+++ admin/src/features/Constants/constants.js
@@ -1,3 +1,3 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'something'
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'https://safe-cliffs-81638.herokuapp.com/api' : 'something'
 
 export const LOCAL_STORATE_TOKEN_NAME = 'token-admin'
